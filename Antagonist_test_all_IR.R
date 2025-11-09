45cdb46bb2ce40b3aef1161d0ffc28d80e7c5b12
kassette <50966326+mirkka-puente@users.noreply.github.com>
2025-11-09 09:52:04 -0500

    Añadir script para ANOVA de ensayos antagonísticos
    Script de R para el análisis de datos de bacterias y hongos patógenos.

---
+++ Antagonist_test_all_IR.R
@@ -0,0 +1,122 @@
+# install.packages(c("multcomp", 'tidyverse', 
+#                    'agricolae', 'ggplot2',
+#                    'car', 'dplyr'))
+
+#Libraries 
+library(multcomp)
+library(tidyverse)
+library(agricolae)
+library(ggplot2)
+library(car)
+library(dplyr)
+
+#Data loading
+setwd('C:/Users/User/Desktop/master_thesis')
+fungal_table <- read.csv('fungal_test.csv')
+bact_table <- read.csv("bacteria.csv")
+
+#Data curation
+fungal_subset <- fungal_table %>% select(strains, pathogen,IR)
+bac_subset <- bact_table %>%  select(strains, pathogen,IR)
+
+#Working table including only strains in thesis project
+inhibition_rate <- rbind(fungal_subset, bac_subset) %>%
+  filter(!strains %in% c("P_protegens", "P_fragi"))
+
+remove(bac_subset, fungal_subset)
+
+# Q-Q plot of data
+ggplot(inhibition_rate, aes(sample = IR)) + 
+  stat_qq() + 
+  stat_qq_line() +
+  ggtitle("Q-Q Plot for Inhibition Rate (IR)")
+
+# Levene's test for homogeneity of variances
+leveneTest(IR ~ pathogen*strains, data = inhibition_rate)
+
+# Shapiro-Wilk test for normality - Fungi
+# Prueba de Shapiro-Wilk for each fungi strain and variable
+by(inhibition_rate$IR, inhibition_rate$strains, shapiro.test)
+by(inhibition_rate$IR, inhibition_rate$pathogen, shapiro.test)
+
+#ANOVA test
+anova_pathogen <- aov(IR ~ strains*pathogen, 
+                      data = inhibition_rate)
+anova_summary <- summary(anova_pathogen)
+
+# Results from ANOVA in a data frame
+anova_table <- anova_summary[[1]]
+anova_p <- as.data.frame(anova_table)
+remove(anova_table, anova_summary)
+
+#Tukey's Honestly Significant Difference test
+hsd <- HSD.test(anova_pathogen,trt = c("strains", "pathogen"),
+                group=TRUE)
+groups <- as.data.frame(hsd$groups)
+remove(hsd)
+#----------------------------------------------------------------
+
+# Fcolmorum <- inhibition_rate %>% filter(pathogen == 'Fusarium_colmorum')
+# model <- aov(IR ~ strains, data = Fcolmorum)
+# HSD_results <- HSD.test(model,trt = c("strains"),group=TRUE)
+# tukey_results <- TukeyHSD(model)
+# print(tukey_results$strains)
+# colmorum_p <- as.data.frame(tukey_results$strains)
+# colmorum <- as.data.frame(HSD_results$groups)
+# remove(model, HSD_results, Fcolmorum, tukey_results)
+# 
+# #--------------------------------------------------------
+# 
+# 
+# Foxysporum <- inhibition_rate %>% 
+# filter(pathogen == 'Fusarium_oxysporum')
+# model <- aov(IR ~ strains, data = Foxysporum)
+# HSD_results <- HSD.test(model,trt = c("strains"),group=TRUE)
+# tukey_results <- TukeyHSD(model)
+# print(tukey_results$strains)
+# oxysporum_p <- as.data.frame(tukey_results$strains)
+# oxysporum <- as.data.frame(HSD_results$groups)
+# remove(model, HSD_results, Foxysporum, tukey_results)
+# 
+# #---------------------------------------------------------
+# 
+# Botrytis_cinerea <- inhibition_rate %>% 
+#   filter(pathogen == 'Botrytis_cinerea')
+# model <- aov(IR ~ strains, data = Botrytis_cinerea)
+# HSD_results <- HSD.test(model,trt = c("strains"),group=TRUE)
+# tukey_results <- TukeyHSD(model)
+# print(tukey_results$strains)
+# cinerea_p <- as.data.frame(tukey_results$strains)
+# cinerea <- as.data.frame(HSD_results$groups)
+# remove(model, HSD_results, Botrytis_cinerea, tukey_results)
+
+#-----------------------------------------------------------
+
+
+# Prepare data for plotting
+mean_all <- inhibition_rate %>%
+  group_by(strains, pathogen) %>%
+  summarize(
+    mean_strains = mean(IR),
+    sem_strains = sd(IR) / sqrt(n()))
+
+
+mean_all$letters <- c('bc', 'c', 'bc', 'b', 'a', 'bc', 'bc', 'bc', 'a', "bc")
+
+# Plot the results with ggplot2
+ggplot(mean_all, aes(x = pathogen, y = mean_strains, fill = strains)) +
+  geom_bar(stat = "identity", position = position_dodge(), width = 0.7) +
+  geom_errorbar(aes(ymin = mean_strains - sem_strains, ymax = mean_strains + sem_strains), 
+                position = position_dodge(0.7), width = 0.25)  +
+  geom_text(aes(label = letters , y = mean_strains + sem_strains + 1), 
+            position = position_dodge(0.7), vjust = 0) +  # Adjust y position for letters
+  theme_minimal() +
+  labs(title = "Pathogen inhibition rate (IR)",
+       x = "Phytopathogens",
+       y = "Inhibition %") +
+  scale_fill_brewer(palette = "Set1")+
+  theme(legend.position = "left")
+
+remove(bact_table, fungal_table, 
+       inhibition_rate, mean_all, anova_pathogen)
+
